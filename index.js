921d31e05af12933c079e8a425add229ba549954
ManuelSong <manuel.song@stonybrook.edu>
2022-07-22 21:35:11 +0900

    practice-TypeScirpt-1

---
+++ index.js
@@ -0,0 +1,16 @@
+var 이름 = 'kim';
+var 이름유니온 = 'kim';
+var 이름어레이 = ['kim', "sam"];
+var 이름오브젝트 = { name: 'kim' }; //{}
+function 함수(x) {
+    return x * 2;
+}
+함수(123);
+var john = [123, true];
+var walker = { name: 'kim', age: '123', "st": '123' };
+var User = /** @class */ (function () {
+    function User(name) {
+        this.name = name;
+    }
+    return User;
+}());
